2d6bd5fb9ea8f6808dd9e37f303fa157c4a62f5d
SandraTsao <s_tsao@fanshaweonline.ca>
2019-03-19 01:26:00 -0400

    new layout after discussion with jarrod and music playing fixed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,98 @@
 (() => {
-		
-	let electronicTracks = document.querySelectorAll(".elecTrack");
+	let dropZone = document.querySelector(".record"),
+		tracks = document.querySelectorAll(".elecTrack"),
+		beat = document.querySelectorAll(".beat"),
+		melody = document.querySelectorAll(".melody"),
+		effect = document.querySelectorAll(".effect"),
+		voice = document.querySelectorAll(".voice"),
+		aud = document.querySelectorAll(".audios");	
 
 	function logMyId() {
-		console.log(this.id);
+		console.log(this.id);  
 		//debugger;
 	}
 
-	electronicTracks.forEach(currenttrack => currenttrack.addEventListener("click", logMyId));
+	tracks.forEach(currenttrack => currenttrack.addEventListener("click", logMyId));
 
-	let dropZone = document.querySelector(".record"),
-		tracks = document.querySelectorAll(".elecTrack");
+	let beat1 = document.querySelector("#beat1"),
+		beat2 = document.querySelector("#beat2"),
+		beat3 = document.querySelector("#beat3"),
+		melody1 = document.querySelector("#melody1"),
+		melody2 = document.querySelector("#melody2"),
+		melody3 = document.querySelector("#melody3"),
+		effect1 = document.querySelector("#effect1"),
+		effect2 = document.querySelector("#effect2"),
+		effect3 = document.querySelector("#effect3"),
+		voice1 = document.querySelector("#voice1"),
+		voice2 = document.querySelector("#voice2"),
+		voice3 = document.querySelector("#voice3");
 
 	function initDrag(){
-		tracks.forEach(img => {
-			img.addEventListener("dragstart", function(e) {
+
+			beat1.addEventListener("dragstart", function(e) {
 				console.log("dragging...");
 
-				e.dataTransfer.setData("images/svg", this.id);
-			});
-		});
+				e.dataTransfer.setData("text", this.id);
+				});
+			beat2.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			beat3.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			melody1.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			melody2.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			melody3.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			effect1.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			effect2.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			effect3.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			voice1.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			voice2.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+			voice3.addEventListener("dragstart", function(e) {
+				console.log("dragging...");
+
+				e.dataTransfer.setData("text", this.id);
+				});
+
+
 	}
+
 
 	dropZone.addEventListener("dragover", function(e) {
 			e.preventDefault();
@@ -30,6 +102,7 @@
 	dropZone.addEventListener("drop", function(e) {
 			e.preventDefault();
 			console.log("you dropped sumpin on me."); 
+			// debugger;
 			
 			let hovered = document.querySelector(".overlay");
 			hovered.classList.add("hovered");
@@ -40,22 +113,58 @@
 			let tuningStick = document.querySelector(".tuneStick");
 			tuningStick.classList.add("stickrotate");
 
-			document.querySelector("audio").play();
 
 			document.querySelectorAll(".bar").forEach(bar => {
 				bar.classList.add("playing");
-			})
-			
-
+			});
 
 		});
 
-
 	initDrag();
+	 // melody.play(); voice.play(); effect.play();
+			beat1.addEventListener("dragend", function() {
+				document.getElementById("beat1A").play();
+				});
+			beat2.addEventListener("dragend", function() {
+				document.getElementById("beat2A").play();
+				});
+			beat3.addEventListener("dragend", function() {
+				document.getElementById("beat3A").play();
+				});
+			melody1.addEventListener("dragend", function() {
+				document.getElementById("melody1A").play();
+				});
+			melody2.addEventListener("dragend", function() {
+				document.getElementById("melody2A").play();
+				});
+			melody3.addEventListener("dragend", function() {
+				document.getElementById("melody3A").play();
+				});
+			effect1.addEventListener("dragend", function() {
+				document.getElementById("effect1A").play();
+				});
+			effect2.addEventListener("dragend", function() {
+				document.getElementById("effect2A").play();
+				});
+			effect3.addEventListener("dragend", function() {
+				document.getElementById("effect3A").play();
+				});
+			voice1.addEventListener("dragend", function() {
+				document.getElementById("voice1A").play();
+				});
+			voice2.addEventListener("dragend", function() {
+				document.getElementById("voice2A").play();
+				});
+			voice3.addEventListener("dragend", function() {
+				document.getElementById("voice3A").play();
+				});
+	
 
 	function stopPlaying() {
 
-		document.querySelector("audio").pause(true);
+		aud.forEach(au => {
+			au.pause();
+		});
 		let hovered = document.querySelector(".overlay");
        		hovered.classList.remove("hovered");
         let dropZone = document.querySelector(".record");
@@ -64,13 +173,11 @@
 			tuningStick.classList.remove("stickrotate");
 		document.querySelectorAll(".bar").forEach(bar => {
 			bar.classList.remove("playing");
-		})
+		});
 
 	}
 
 	let stick = document.querySelector("#stop");
 	stick.addEventListener("click", stopPlaying);
-
-	stopPlaying();
 
 })(); 
